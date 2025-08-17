a23cabb8b2708ca7d77dbb4e14b8a86a451cc505
IStasovskyi93 <stsovsky400m@gmail.com>
2025-08-17 18:56:59 +0200

    module and intagration tests and ESlint

--- src/meadowlark.js
+++ src/meadowlark.js
@@ -3,6 +3,7 @@ const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
 const fortune = require("../lib/fortune");
+const handlers = require("../lib/handlers");
 
 //configurate mechanism views of Handlebars
 app.engine(
@@ -16,27 +17,22 @@ app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune() });
-});
+app.get("/about", handlers.about);
 
 //user page 404
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
 //use page 500
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
-app.listen(port, () =>
-  console.log(
-    `Express run on http://localhost:${port}` + "\npress Ctrl+C for ending"
-  )
-);
+if (require.main === module) {
+  app.listen(port, () =>
+    console.log(
+      `Express run on http://localhost:${port}` + "\npress Ctrl+C for ending"
+    )
+  );
+} else {
+  module.exports = app;
+}
